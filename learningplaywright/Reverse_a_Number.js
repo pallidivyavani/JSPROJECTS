1d75b7454d0c44ad1650006487922b808a7c8460
pallidivyavani <divyavanipalli7036@gmail.com>
2026-09-22 17:29:32 +0530

    Add files via upload

--- learningplaywright/Reverse_a_Number.js
+++ learningplaywright/Reverse_a_Number.js
@@ -1,8 +1,13 @@
-let a = 1234;
-let reverse = 0;
-while (a>0){
-    let digit = a % 10;
-    reverse = reverse * 10 + digit ;
-    a= Math.floor(a/10);
-}
-console.log(reverse);
+// let a = 1234;
+// let reverse = 0;
+// while (a>0){
+//     let digit = a % 10;
+//     reverse = reverse * 10 + digit ;
+//     a= Math.floor(a/10);
+// }
+// console.log(reverse);
+
+
+let a = ["1", "2", "3", "4"];
+let reversed = a.reverse();
+console.log(reversed);
