330145769da01d7d96332dbb23a49842eabeb3a8
pallidivyavani <divyavanipalli7036@gmail.com>
2026-09-24 18:34:56 +0530

    Add files via upload

---
+++ learningplaywright/array_Count_how_many_numbers_are_greater_than.js
@@ -0,0 +1,8 @@
+let numbers = [25, 75, 40, 90, 60, 10];
+
+let count = 0;
+for(let i=0; i < numbers.length ; i++){
+    if(numbers[i]> 50){
+        count++;
+    }
+}console.log(count);
--- learningplaywright/array_largest_number.js
+++ learningplaywright/array_largest_number.js
@@ -1,7 +1,19 @@
+// let numbers = [15, 72, 34, 91, 28];
+// let largest = numbers[0];
+// for(let num of numbers){
+//     if(num > largest){
+//         largest = num;
+//     }
+// }console.log(largest)
+
+
+
 let numbers = [15, 72, 34, 91, 28];
-let largest = numbers[0];
-for(let num of numbers){
-    if(num > largest){
-        largest = num;
-    }
-}console.log(largest)
+
+let count = numbers[0];
+for(let i = 1; i < numbers.length; i++){
+  if(numbers[i] > count){
+    count = numbers[i]
+  }
+}
+console.log('Biggesst number is '+ count)
